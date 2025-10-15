9ea54d7b69ee23d1fffab2a1f70805590d33969a
betsy <betsykawira52@gmail.com>
2025-10-15 08:34:53 +0300

    Add files via upload

---
+++ user-input-and-out.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout<< "what is your name?\n";
+    string name;
+    cin>> name;
+    cout << "your name is " <<name<< "\n";
+    system("pause");
+    return 0;
+}
